f6f51ea3745ba1419414d52a0747120e1d5e6e8b
KHAIRI AHMAD (DCAZ/SLF-MY) <ahk1shm@bosch.com>
2021-09-19 23:46:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//practice with declaring variables with values
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+const luckyNum = 27;
+const boilingPointC = 100;
+const boilingPointF = 212;
+
+//performing math calculations using math operators
+myLuckyNumber = myLuckyNumber + 1;
+myLuckyNumber += 1;
+myLuckyNumber++;
+
+10 % 3; //where % is the remainder operator
+200 + NaN; //where NaN is Not a Number
+3 ** 3; //where ** is the exponent or power of operator
+
+//quizQ1
+let score = 5;
+score + 4; //answer is still 5 because we did not recall the latest value of score
+
+//quizQ2
+let bankBalance = 100;
+bankBalance /= 2;   //answer is 50
+bankBalance += 10; //final answer is 60
+
+//quizQ3
+const age = 64;
+age++; //answer will be 64 because because we cannot reassign a const variable. 
